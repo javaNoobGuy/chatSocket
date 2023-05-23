35a7d91eb61a9ebd6cab5b160d6307176db52f6f
JavaNoobGuy <116377217+javaNoobGuy@users.noreply.github.com>
2023-05-23 10:12:35 -0300

    all

---
+++ server.js
@@ -0,0 +1,30 @@
+const app = require('express')();
+
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+
+io.on('connection', (socket) => {console.log('usuário conectado id da conexão ',socket.id )
+
+    socket.on('disconnect', (socket) => console.log('you was diconnected',socket))
+
+    socket.on('chat message', (socket) => {console.log(socket);io.emit('chat message', socket);});
+
+    socket.on('sendItToServer', (msg) =>{
+
+        console.log(msg);
+        socket.broadcast.emit('sendItToServer', msg);
+
+    })
+
+})
+
+
+
+http.listen(3000, () =>{
+    console.log("fdsfsdf");
+})
+
+
