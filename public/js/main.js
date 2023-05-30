b858d8f2de362b1bcb8f309cb550c367ba0b4108
javaNoobGuy <116377217+javaNoobGuy@users.noreply.github.com>
2023-05-30 14:02:08 +0000

    fddfd

--- public/js/main.js
+++ public/js/main.js
@@ -1,66 +1,66 @@
 var socket = io();
-if(localStorage.getItem('nomeUser') == undefined){
+if (localStorage.getItem('nomeUser') == undefined) {
 
-    localStorage.setItem('nomeUser','');
+  localStorage.setItem('nomeUser', '');
 
 }
 
 messages = [];
 
-function login(){
+function login() {
 
-    let nome = document.getElementById('nome').value;
-    socket.emit('loginMade',{nome});
+  let nome = document.getElementById('nome').value;
+  socket.emit('loginMade', { nome });
 
 };
 
-    socket.on('isNameValid', (res) =>{
-        let nome = document.getElementById('nome').value;
-        if(!res){
-            alert('nome já usado amigo ;)');
-        }else{
-            localStorage.setItem('nomeUser',nome);
-            socket.emit('addUser',{nome});
-            location.href = "talk.html";
-        }});
-
-if(location.href == "http://localhost:3000/talk.html"){
-    socket.emit('getMessages');
-    console.log('requsisção de mensagens');
-}
-
-socket.on('update', (data) =>{
-    messages = data;
-    console.log(messages);
-    renderM();
+socket.on('isNameValid', (res) => {
+  let nome = document.getElementById('nome').value;
+  if (!res) {
+    alert('nome já usado amigo ;)');
+  } else {
+    localStorage.setItem('nomeUser', nome);
+    socket.emit('addUser', { nome });
+    location.href = "talk.html";
+  }
 });
 
-function renderM(){
-    let mensagens = document.getElementById('campoMensagem');
-    mensagens.innerHTML = "";
-    for(let i = 0; i < messages.length;i++){
-        console.log('desenhando a ');
-        console.log(messages[i]);
-        
-        let mensagemAtual = document.createElement('div');
-        let span = document.createElement('span');
-        span.innerText = messages[i].content;
-        mensagemAtual.append(span);
-        if(messages[i].owner.nom 
-           e == localStorage.getItem('nomeUser')){
-            mensagemAtual.className = 'userText';
-        }else{
-            mensagemAtual.className = 'othersText';
-        }
-        mensagens.append(mensagemAtual);
-    }
+if (location.href == "http://localhost:3000/talk.html") {
+  socket.emit('getMessages');
+  console.log('requsisção de mensagens');
 }
 
-function send(){
-    let content = document.getElementById('mensagem').value;
-    let nome = localStorage.getItem('nomeUser');
-    console.log('enviando mensagens ao servidor');
-    socket.emit('sendMessage', {content, nome});
+socket.on('update', (data) => {
+  messages = data;
+  console.log(messages);
+  renderM();
+});
+
+function renderM() {
+  let mensagens = document.getElementById('campoMensagem');
+  mensagens.innerHTML = "";
+  for (let i = 0; i < messages.length; i++) {
+    console.log('desenhando a ');
+    console.log(messages[i]);
+
+    let mensagemAtual = document.createElement('div');
+    let span = document.createElement('span');
+    span.innerText = messages[i].content;
+    mensagemAtual.append(span);
+    if (messages[i].owner.nome == localStorage.getItem('nomeUser')) {
+      mensagemAtual.className = 'userText';
+    } else {
+      mensagemAtual.className = 'othersText';
+    }
+    mensagens.append(mensagemAtual);
+  }
+}
+
+function send() {
+  let content = document.getElementById('mensagem').value;
+  let nome = localStorage.getItem('nomeUser');
+  console.log('enviando mensagens ao servidor');
+  socket.emit('sendMessage', { content, nome });
 
 }
 
